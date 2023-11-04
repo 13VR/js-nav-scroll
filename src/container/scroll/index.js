7590185135712572eca234b12d1cd441fa4ec903
Vitaliy Radchenko <rad4enko.vitaliy@gmail.com>
2023-11-04 15:18:02 +0200

    nav-scroll

---
+++ src/container/scroll/index.js
@@ -0,0 +1,31 @@
+window.vector.onclick = () =>
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  })
+
+let isDisplay = false
+
+setInterval(() => {
+  if (
+    window.scrollY > window.innerHeight &&
+    isDisplay === false
+  ) {
+    isDisplay = true
+
+    window.vector.style.display = 'flex'
+
+    return
+  }
+  if (
+    window.scrollY < window.innerHeight &&
+    isDisplay === true
+  ) {
+    isDisplay = false
+
+    window.vector.style.display = 'none'
+
+    return
+  }
+}, 500)
